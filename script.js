2aa6248d6051d66673324c2b616ffff412ea7b73
Your Name <ziaveer106@gmail.com>
2021-08-01 15:37:04 +0530

    initial

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("ll");
+// const url = "hello.txt";
+// console.log(url);
+fetch('https://swapi.dev/api/people/1/')
+  .then((response) => {
+      console.log(response);
+    //   console.log(response.json());
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data); 
+  });
